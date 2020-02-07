724be5e78cda4292967b6c2b53cb82b193673353
Francisco do Vale <francisco_do_vale@epam.com>
2020-02-07 11:01:01 +0100

    push method modification and new tests

--- studies/linked-list.js
+++ studies/linked-list.js
@@ -14,21 +14,51 @@ class LinkedList {
     this.length = 0;
     this.head = undefined;
     this.equalsFn = equalsFn;
+	}
+	
+  insertAtPosition(node, position) {
+    let current;
+    if (position === 0) {
+      current = this.head;
+      node.next = current;
+      this.head = node;
+    } else {
+      const previous = this.getElementAt(position - 1);
+      current = previous.next;
+      node.next = current;
+      previous.next = node;
+    }
+
+		this.length++;
   }
 
-  push(element) {
-    const node = new Node(element);
+  insertOnTail(node) {
     let current;
-
     if (this.head == null) {
       this.head = node;
     } else {
       current = this.head;
       while (current.next != null) {
         current = current.next;
-      }
+			}
+			
+			current.next = node;
     }
     this.length++;
+  }
+
+  push(element, index = null) {
+    const node = new Node(element);
+
+    if (index == null) {
+      return this.insertOnTail(node);
+    }
+
+    if (index >= 0 && index <= this.length) {
+			return this.insertAtPosition(node, index);
+    }
+
+    return false;
   }
 
   removeAt(index) {
@@ -46,7 +76,7 @@ class LinkedList {
       return current.element;
     }
 
-    return undefined;
+    return null;
   }
 
   getElementAt(index) {
@@ -57,7 +87,7 @@ class LinkedList {
       }
       return node;
     }
-    return undefined;
+    return null;
   }
 }
 
--- studies/test/linked-list.spec.js
+++ studies/test/linked-list.spec.js
@@ -2,7 +2,7 @@ const LinkedList = require('../linked-list');
 
 test('creates a new list', async () => {
   const list = new LinkedList();
-  expect(list).toBeTruthy(); 
+  expect(list).toBeTruthy();
 });
 
 test('check empty list', async () => {
@@ -10,10 +10,26 @@ test('check empty list', async () => {
   expect(list.head).toBeFalsy();
 });
 
-test('insert a item on list', async () => {
+test('insert items on a list', async () => {
   const list = new LinkedList();
   list.push(25);
-  expect(list.length).toEqual(1);
+  list.push('foo');
+  list.push({ a: 'bar' });
+  expect(list.length).toEqual(3);
+});
+
+test('insert a item on a specific position', async () => {
+  const list = new LinkedList();
+  list.push(25, 0);
+  list.push('test');
+  list.push(16, 1);
+  expect(list.getElementAt(1).element).toEqual(16);
+});
+
+test('insert a item on list at a not valid position', async () => {
+  const list = new LinkedList();
+  list.push(25, 2);
+  expect(list.length).toBeFalsy();
 });
 
 test('check not empty list', async () => {
