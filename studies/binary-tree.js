90ec5374a2bcc0e4d46b52482fa798081151d29d
Francisco do Vale <francisco_do_vale@epam.com>
2020-02-10 17:19:47 +0100

    Create tree from array

--- studies/binary-tree.js
+++ studies/binary-tree.js
@@ -1,26 +1,55 @@
 // https://www.geeksforgeeks.org/construct-complete-binary-tree-given-array/
 
-
-// A full binary tree.is a binary tree in which each node has exactly zero or two children.
-
-// A complete binary tree is a binary tree, which is completely filled, with the possible exception of the bottom level, which is filled from left to right.
-
 class Node {
-  constructor() {
+  constructor(data = null, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
     this.parent = null;
-    this.left = null;
-    this.right = null;
-    this.depth = 0;
-    this.height = 0;
   }
 }
 
 class Tree {
   constructor() {
-    this.length = 0;
     this.root = null;
-    this.heigth = 0;
+  }
+
+  create(list) {
+
+    for (let i = 0; i < list.length; i++) {
+      const node = new Node(list[i]);
+      let current;
+
+      if (this.root == null) {
+        this.root = node;
+      } else {
+        current = this.root;
+        while(true) {
+          if (list[i] < current.data) {
+            if (current.left) {
+              current = current.left;
+            } else {
+              current.left = node;
+              break;
+            }
+          } else {
+            if (current.right) {
+              current = current.right;
+            } else {
+              current.right = node;
+              break;
+            }
+          }
+        }
+      }
+    }
+    return this;
   }
 
 
 }
+
+const list = [11, 6, 8, 19, 4, 10, 5, 17, 43, 49, 31];
+const tree1 = new Tree();
+tree1.create(list);
+console.log(JSON.stringify(tree1, null, 4));
