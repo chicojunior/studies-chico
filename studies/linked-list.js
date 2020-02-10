b10fe9910a6f4384670e314246be429ca63f3fe1
Francisco do Vale <francisco_do_vale@epam.com>
2020-02-10 14:13:18 +0100

    Implementation of the toString method and the CircularLinkedList

--- studies/linked-list.js
+++ studies/linked-list.js
@@ -102,6 +102,73 @@ class LinkedList {
     }
     return null;
   }
+
+  toString() {
+    if (this.head == null) {
+      return '';
+    }
+
+    let objString = `${this.head.element}`;
+    let current = this.head.next;
+
+    for (let i = 0; i < this.length && current != null; i++) {
+      objString = `${objString},${current.element}`;
+      current = current.next;
+    }
+
+    return objString;
+  }
+}
+
+class CircularLinkedList extends LinkedList {
+  constructor() {
+    super();
+  }
+  insertAtPosition(node, position) {
+    let current = this.head;
+    if (position === 0) {
+      if (this.head == null) {
+        this.head = node;
+        node.next = this.head;
+      } else {
+        node.next = current;
+        current = this.getElementAt(this.length);
+        this.head = node;
+        current.next = this.head;
+      }
+    } else {
+      const previous = this.getElementAt(position - 1);
+      current = previous.next;
+      node.next = current;
+      previous.next = node;
+    }
+
+    this.length++;
+  }
+
+  removeAt(index) {
+    if (index >= 0 && index < this.count) {
+      let current = this.head;
+      if (index === 0) {
+        if (this.length === 1) {
+          this.head = undefined;
+        } else {
+          const removed = this.head;
+          current = this.getElementAt(this.length);
+          this.head = this.head.next;
+          current.next = this.head;
+          current = removed;
+        }
+      } else {
+        const previous = this.getElementAt(index - 1);
+        current = previous.next;
+        previous.next = current.next;
+      }
+      this.count--;
+      return current.element;
+    }
+    return null;
+  }
 }
 
 module.exports = Node;
